ed05b4ccfd8b1350ae2e425cee76ab97f80e29ad
poorvi25 <poorvishrivastav80@gmail.com>
2025-11-09 23:27:40 +0530

    week 3 day 3 pagination+sorting & decorator in LWC

--- force-app/main/default/lwc/accountSearch/accountSearch.js
+++ force-app/main/default/lwc/accountSearch/accountSearch.js
@@ -5,11 +5,22 @@ export default class AccountSearch extends LightningElement {
     @track industry = '';
     @track accounts;
     @track error;
+    
+    // Pagination state
+    pageSize = 10;
+    offsetVal = 0;
+    disableNext = false; //for next button
+    disablePrev = true; //for prev button
+
+    // default Sorting state
+    sortField = 'Name';
+    sortDirection = 'ASC';
 
     columns = [
-        { label: 'Name', fieldName: 'Name' },
-        { label: 'Industry', fieldName: 'Industry' },
-        { label: 'Phone', fieldName: 'Phone' }
+        { label: 'Name', fieldName: 'Name', sortable: true },
+        { label: 'Industry', fieldName: 'Industry', sortable: true },
+        { label: 'Phone', fieldName: 'Phone', sortable: true },
+        { label: 'Phone', fieldName: 'Phone', sortable: true }
     ];
 
     handleChange(event) {
@@ -17,22 +28,50 @@ export default class AccountSearch extends LightningElement {
     }
 
     handleSearch() {
-        this.isLoading = true;   // 🌀 show spinner
+       this.offsetVal = 0;
+        this.fetchAccounts();
+    }
+fetchAccounts() {
+        this.isLoading = true; //show spinner
         this.error = undefined;
-        this.accounts = undefined;
-
-        getAccountsByIndustry({ industry: this.industry })
+        getAccountsByIndustry({
+            industry: this.industry,
+            offsetVal: this.offsetVal,
+            limitSize: this.pageSize,
+            sortField: this.sortField,
+            sortDirection: this.sortDirection
+        })
             .then(result => {
-                this.isLoading = false;  // hide spinner
-                if (result.length > 0) {
-                    this.accounts = result;
-                } else {
+                this.isLoading = false;
+                this.accounts = result;
+                this.disablePrev = this.offsetVal === 0;
+                this.disableNext = result.length < this.pageSize;
+                if (result.length === 0) {
                     this.error = 'No Accounts Found.';
                 }
             })
             .catch(error => {
                 this.isLoading = false;
                 this.error = error.body.message;
+                this.accounts = undefined;
             });
+    }
+//Pagiation buttons
+    handleNext() {
+        this.offsetVal += this.pageSize;
+        this.fetchAccounts();
+    }
+
+    handlePrev() {
+        if (this.offsetVal >= this.pageSize) {
+            this.offsetVal -= this.pageSize;
+            this.fetchAccounts();
+        }
+    }
+
+    handleSort(event) {
+        this.sortField = event.detail.fieldName;
+        this.sortDirection = event.detail.sortDirection.toUpperCase();
+        this.fetchAccounts();
     }
 }
